b2d8b144578a017ae3b16b575a42af6ffec4ccf9
Manabu Matsuzaki <matsumana@gmail.com>
2021-05-29 19:41:04 +0900

    implement contentService#getHistory

--- test/contentService.test.ts
+++ test/contentService.test.ts
@@ -411,3 +411,259 @@ describe('ContentService#getFile', () => {
         });
     });
 });
+
+describe('ContentService#getHistory with nested directory', () => {
+    it('get HEAD revision without "from" from "/**"', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            to: 'HEAD',
+        });
+
+        // if you call the function without "from", it returns one commit
+        expect(entries.length).toBe(1);
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+    });
+
+    it('get specific revision without "from" from "/**"', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            to: '4',
+        });
+
+        // if you call the function without "from", it returns one commit
+        expect(entries.length).toBe(1);
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+    });
+
+    it('get between specific revision and HEAD from "/**"', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            from: '1',
+            to: 'HEAD',
+        });
+
+        // if you call the function with "from", it returns multiple commits
+        expect(entries.length).toBe(3);
+
+        // 1st
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+
+        // 2nd
+        expect(entries[1].revision).toBe(3);
+        expect(entries[1].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[1].commitMessage).toEqual({
+            summary: 'Add /test4.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[1].pushedAt).toBeTruthy();
+
+        // 3rd
+        expect(entries[2].revision).toBe(2);
+        expect(entries[2].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[2].commitMessage).toEqual({
+            summary: 'Add /test3.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[1].pushedAt).toBeTruthy();
+    });
+
+    it('get between specific revision and specific revision from "/**"', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            from: '1',
+            to: '4',
+        });
+
+        // if you call the function with "from", it returns multiple commits
+        expect(entries.length).toBe(3);
+
+        // 1st
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+
+        // 2nd
+        expect(entries[1].revision).toBe(3);
+        expect(entries[1].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[1].commitMessage).toEqual({
+            summary: 'Add /test4.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[1].pushedAt).toBeTruthy();
+
+        // 3rd
+        expect(entries[2].revision).toBe(2);
+        expect(entries[2].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[2].commitMessage).toEqual({
+            summary: 'Add /test3.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[1].pushedAt).toBeTruthy();
+    });
+
+    it('get HEAD revision from the specific directory', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            pathPattern: '/dir1/*',
+            to: 'HEAD',
+        });
+
+        // if you call the function without "from", it returns one commit
+        expect(entries.length).toBe(1);
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+    });
+
+    it('get between specific revision and HEAD from the specific directory', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            pathPattern: '/dir1/*',
+            from: '1',
+            to: 'HEAD',
+        });
+
+        // if you call the function with "from", it returns multiple commits
+        expect(entries.length).toBe(2);
+
+        // 1st
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+
+        // 2nd
+        expect(entries[1].revision).toBe(3);
+        expect(entries[1].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[1].commitMessage).toEqual({
+            summary: 'Add /test4.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[1].pushedAt).toBeTruthy();
+    });
+
+    it('get HEAD revision from the specific file', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            pathPattern: '/dir1/test5.json',
+            to: 'HEAD',
+        });
+
+        // if you call the function without "from", it returns one commit
+        expect(entries.length).toBe(1);
+        expect(entries[0].revision).toBe(4);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test5.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+    });
+
+    it('get specific revision from the specific file', async () => {
+        const entries = await sut.getHistory({
+            project: 'project1',
+            repo: 'repo2',
+            pathPattern: '/dir1/test4.json',
+            to: '3',
+        });
+
+        // if you call the function without "from", it returns one commit
+        expect(entries.length).toBe(1);
+        expect(entries[0].revision).toBe(3);
+        expect(entries[0].author).toEqual({
+            name: 'admin',
+            email: 'admin@localhost.localdomain',
+        });
+        expect(entries[0].commitMessage).toEqual({
+            summary: 'Add /test4.json',
+            detail: '',
+            markup: 'PLAINTEXT',
+        });
+        expect(entries[0].pushedAt).toBeTruthy();
+    });
+});
